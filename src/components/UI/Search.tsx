c3c85841f5bef80f8f24e99eb6381327ce8c4717
dudamaciej88 <dudamaciej88@gmail.com>
2020-09-03 21:16:58 +0200

    Add component

---
+++ src/components/UI/Search.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+type Props = {
+  getQuery: (q: string) => void;
+};
+
+const Search: React.FC<Props> = ({ getQuery }) => {
+  const [text, setText] = useState("");
+
+  const onChange = (q: string) => {
+    setText(q);
+    getQuery(q);
+  };
+
+  return (
+    <section className="search">
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search characters"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+};
+
+export default Search;
