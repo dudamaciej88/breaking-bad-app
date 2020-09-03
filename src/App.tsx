992151da4a000a90fd56dc9c295f9367fd9f0ee4
dudamaciej88 <dudamaciej88@gmail.com>
2020-09-03 21:18:09 +0200

    Add query

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,29 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./components/UI/Header";
 import CharacterGrid from "./components/Characters/CharacterGrid";
+import Search from "./components/UI/Search";
 import "./App.css";
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://breakingbadapi.com/api/characters`);
+      const result = await axios(
+        `https://breakingbadapi.com/api/characters?name=${query}`
+      );
       setItems(result.data);
       setIsLoading(false);
-      console.log(result.data);
     };
     fetchItems();
-  },[]);
+  }, [query]);
 
   return (
     <div className="App">
       <Header />
+      <Search getQuery={(q: string) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
